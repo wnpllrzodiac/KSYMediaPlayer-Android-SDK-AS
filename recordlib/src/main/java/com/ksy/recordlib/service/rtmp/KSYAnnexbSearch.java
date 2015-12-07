c5d3a8bc93817ea239ccf30cb7c50359bae5dbcf
12345nbaa <zzpblxp@126.com>
2015-12-07 16:19:21 +0800

    delete recordlib
    delete recordlib

--- recordlib/src/main/java/com/ksy/recordlib/service/rtmp/KSYAnnexbSearch.java
+++
@@ -1,7 +0,0 @@
-package com.ksy.recordlib.service.rtmp;
-
-public class KSYAnnexbSearch {
-
-	public int nb_start_code = 0;
-	public boolean match = false;
-}
--- recordlib/src/main/java/com/ksy/recordlib/service/util/OrientationActivity.java
+++
@@ -1,9 +0,0 @@
-package com.ksy.recordlib.service.util;
-
-import android.app.Activity;
-
-public interface OrientationActivity {
-    int getOrientation();
-
-    Activity getActivity();
-}
